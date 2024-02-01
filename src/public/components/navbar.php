b1fd9206f291a6a0b3474065a5e46e62cefb390f
Damien Vergobbi <damien.vergobbi.pro@gmail.com>
2024-02-01 12:11:16 +0100

    Create editor and settings page ; add 'active' class to navbar

--- src/public/components/navbar.php
+++ src/public/components/navbar.php
@@ -2,18 +2,23 @@
   Get const IS_LOGGED from index.php
 -->
 
+<?php
+// Get current route
+$pagename = basename($_SERVER['REQUEST_URI']);
+?>
+
 <nav>
   <div class="nav-wrapper">
     <a href="index.php" class="brand-logo">Camagru</a>
     <ul id="nav-mobile" class="right hide-on-med-and-down">
       <li>
-        <a href="editor.php">
+        <a href="editor.php" class="<? echo $pagename === "editor.php" ? 'active' : '' ?>">
           <img src="../../app/media/icon-editor.png" height="30" />
           Editor
         </a>
       </li>
       <li>
-        <a href="index.php">
+        <a href="index.php" class="<? echo $pagename === "index.php" ? 'active' : '' ?>">
           <img src="../../app/media/icon-gallery.png" height="30" />
           Gallery
         </a>
@@ -22,7 +27,7 @@
 
     <?php if (IS_LOGGED): ?>
       <div class="user-settings">
-        <a href="settings.php" class="settings">
+        <a href="settings.php" class="settings <? echo $pagename === "settings.php" ? 'active' : '' ?>">
           <img src="../../app/media/icon-settings.png" height="30" />
           <? echo $_SESSION['user_name'] ?>
         </a>
---
+++ src/public/settings.php
@@ -0,0 +1,28 @@
+<?php // Redirect to signin.php if the user is not logged in
+session_start();
+
+define('IS_LOGGED', isset($_SESSION['user_id'])
+    && isset($_SESSION['user_name'])
+    && isset($_SESSION['user_email']));
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Camagru - Settings</title>
+
+    <link rel="stylesheet" href="../app/css/global.css">
+    
+    <!-- Load style from ../app/css/signin.css -->
+    <link rel="stylesheet" href="../app/css/navbar.css">
+</head>
+<body>
+    <? require_once 'components/navbar.php'; ?>
+
+    <h1>Welcome to the settings page</h1>
+
+</body>
+</html>
