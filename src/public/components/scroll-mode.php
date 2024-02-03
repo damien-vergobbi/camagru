2b21a6a7133fcef6cf01fa589d5647c1db2b496c
Damien Vergobbi <damien.vergobbi.pro@gmail.com>
2024-02-03 19:17:51 +0100

    Add share buttons and responsive on post page

--- src/public/components/scroll-mode.php
+++ src/public/components/scroll-mode.php
@@ -6,11 +6,11 @@ $pagename = basename($_SERVER['REQUEST_URI']);
 <aside id="scroll-mode">
     <a href="index.php" class="<? echo $pagename !== "infinite.php" ? 'active' : '' ?>">
         <img src="../../app/media/icon-pages.png" height="30" />
-        Pages
+        Pages mode
     </a>
 
     <a href="infinite.php" class="<? echo $pagename === "infinite.php" ? 'active' : '' ?>">
         <img src="../../app/media/icon-infinity.png" height="30" />
-        Infinity
+        Infinite scroll
     </a>
 </aside>
--- src/public/post.php
+++ src/public/post.php
@@ -25,6 +25,14 @@ define('IS_LOGGED', isset($_SESSION['user_id'])
     <? require_once 'components/navbar.php'; ?>
 
     <div class="infinite-wrapper">
+
+      <aside id="scroll-mode" class="lefter">
+        <a href="index.php" id="back-to-feed">
+          <img src="../../app/media/icon-return.png" height="30" />
+          Back to feed
+        </a>
+      </aside>
+
       <main>
 
         <div id="post" class="item-div hidden">
@@ -34,6 +42,7 @@ define('IS_LOGGED', isset($_SESSION['user_id'])
               <img src="../app/media/icon-user.png" alt="User" height="20" width="20">
               <p id="post_username">loading</p>
             </div>
+
             <div class="footer-end">
               <div>
                 <img src="../app/media/icon-like.png" alt="Like" height="20">
@@ -44,6 +53,21 @@ define('IS_LOGGED', isset($_SESSION['user_id'])
                 <p id="post_comments">0</p>
               </div>
             </div>
+          </div>
+
+          <div id="share-div">
+            <p>Share this post</p>
+            <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); ?>" target="_blank">
+              <img src="../app/media/logo-facebook.svg" alt="Facebook" height="20">
+            </a>
+
+            <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); ?>" target="_blank">
+              <img src="../app/media/logo-linkedin.png" alt="Linkedin" height="20">
+            </a>
+
+            <a href="mailto:?subject=Camagru Share&body=See this post : <?php echo urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']); ?>">
+              <img src="../app/media/logo-mails.png" alt="Email" height="20">
+            </a>
           </div>
 
           <?php if (IS_LOGGED): ?>
@@ -73,20 +97,9 @@ define('IS_LOGGED', isset($_SESSION['user_id'])
       </div>
     </div>
 
-
-    <aside id="scroll-mode">
-      <a href="index.php" id="back-to-feed">
-        <img src="../../app/media/icon-pages.png" height="30" />
-        Back to feed
-      </a>
-    </aside>
-
-
     <script>
       let isLoading = false;
       let postId = window.location.search.split('=')[1];
-
-      console.log('Post ID:', postId);
 
       const hideLoader = () => {
         isLoading = false;
