f79def5fbf7cd16228a1348ea42e64df96e5e5c6
Damien Vergobbi <damien.vergobbi.pro@gmail.com>
2024-02-05 14:18:33 +0100

    Fix front stickers ; sort by date

--- src/public/components/stickers-list.php
+++ src/public/components/stickers-list.php
@@ -10,6 +10,11 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_name']) || !isset($_S
 $stickers = array_diff(scandir('../app/stickers'), array('..', '.'));
 $checked = true;
 
+// Sort by creation date
+usort($stickers, function ($a, $b) {
+  return filemtime('../app/stickers/' . $a) > filemtime('../app/stickers/' . $b) ? 1 : -1;
+});
+
 echo '<div id="stickers_list">';
 foreach ($stickers as $sticker) {
   // Radio button with sticker
